3babfae20dfd16f26210066f27be87d2c743b04e
louix <28579246+louix@users.noreply.github.com>
2021-05-11 12:59:54 +0100

    Simplify dunst toggle

--- .scripts/dunst-status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# shell script to get mic status
-
-PAUSED=`dunstctl is-paused`
-if [ $PAUSED == "true" ]; then
-    echo '{"icon": "bell-slash", "state": "Warning", "text": "Focus Mode"}' || exit 1
-else
-    echo '{"icon": "bell", "state": "Good", "text": ""}' || exit 1
-fi
