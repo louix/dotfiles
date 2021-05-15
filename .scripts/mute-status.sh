b987dae804a82cdae8f04431a480a3085b2665e0
louix <28579246+louix@users.noreply.github.com>
2021-05-15 08:28:23 +0100

    i3status-rust update

--- .scripts/mute-status.sh
+++ .scripts/mute-status.sh
@@ -3,7 +3,7 @@
 
 MUTE=`pacmd list-sources | grep "\*" -A 15 | grep "muted" | cut -f 2 | cut -d " " -f 2`
 if [ $MUTE == "yes" ]; then
-    echo '{"icon": "microphone_muted", "state": "Warning", "text": "MUTE"}' || exit 1
+    echo '{"icon": "microphone_muted", "state": "Warning", "text": ""}' || exit 1
 else
-    echo '{"icon": "microphone_full", "state": "Good", "text": "LIVE"}' || exit 1
+    echo '{"icon": "microphone_full", "state": "Good", "text": ""}' || exit 1
 fi
