bb17b73c579f70a4b2b12e195b432285989c8258
louix <louis@otsuka.re>
2021-05-28 14:30:04 +0100

    Update color picker

---
+++ .scripts/colour-picker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CHAMELEON=$(which chameleon)
+
+if [ '$CHAMELEON' ]; then
+    RUNNING=$(pgrep chameleon)
+    if [ '$RUNNING' ]; then
+        pkill chameleon
+    fi
+    chameleon
+    #pgrep chameleon || chameleon
+    #pkill chameleon && chameleon
+fi
+
