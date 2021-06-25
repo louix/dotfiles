f71ba5a2f4e83f6dbff7e4f176c2d71ba83fe55a
louix <louis@otsuka.re>
2021-06-25 12:20:16 +0100

    Fix audio status

--- .scripts/mute-status-pw.sh
+++ .scripts/mute-status-pw.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # shell script to get mic status
 
-MUTE=`pw-cli e 48 Props | grep 'mute' -A1 | grep 'true'`
+SINK=`pactl info | grep Source | cut -d ' ' -f 3`
+NODE_ID=`pw-cli ls Node | grep $SINK -B 10 | grep -E 'id [0-9]{2}' | cut -d ' ' -f 2 | cut -d ',' -f 1`
+MUTE=`pw-cli e $NODE_ID Props | grep 'mute' -A1 | grep 'true'`
 if [ -n "$MUTE" ]; then
     echo '{"icon": "microphone_muted", "state": "Warning", "text": ""}' || exit 1
 else
