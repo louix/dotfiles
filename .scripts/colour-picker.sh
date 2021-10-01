fffc360fa3c9ae49e8ca9ec6c2ce42648870a8c0
louix <louis@otsuka.re>
2021-10-01 08:48:29 +0100

    Simpler color picker]

--- .scripts/colour-picker.sh
+++ .scripts/colour-picker.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-
-CHAMELEON=$(which chameleon)
-
-if [ '$CHAMELEON' ]; then
-    RUNNING=$(pgrep chameleon)
-    if [ '$RUNNING' ]; then
-        pkill chameleon
-    fi
-    chameleon
-    #pgrep chameleon || chameleon
-    #pkill chameleon && chameleon
-fi
-
+xcolor | xclip -se c
+# CHAMELEON=$(which chameleon)
+# 
+# if [ '$CHAMELEON' ]; then
+    # RUNNING=$(pgrep chameleon)
+    # if [ '$RUNNING' ]; then
+        # pkill chameleon
+    # fi
+    # chameleon
+    # #pgrep chameleon || chameleon
+    # #pkill chameleon && chameleon
+# fi
+# 
